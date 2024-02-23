d674e4ff59364f0a0b424211eb44f88b2863d61d
Muhammad Kashif <gmdcf797@gmail.com>
2024-02-23 19:22:23 +0500

    bug-fixed

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,15 +2,18 @@ import PopularMovies from "../../components/popularmovies/PopularMovies";
 export default function Home() {
   return (
     <>
-      <div className="flex py-5">
+      <div className="flex justify-between py-5">
         <div className="text-black text-xl font-medium xx:w-10 sm:w-10 xx:py-3 sm:py-3 md:w-auto ">
           <h5 className="ml-20 py-3 xx:py-3 sm:py-3 md:py-2">Popular Movies</h5>
         </div>
-        <div className="md:py-5 md:ml-24 lg:-ml-32 xl:ml-0 ">
-          <h5 className="lg:ml-[460px] xl:ml-[340px] text-black text-xl font-medium  xx:hidden sm:hidden md:hidden lg:block">
+        <div className="md:py-5 ml-10 ">
+          <h5 className=" text-black text-xl font-medium  xx:hidden sm:hidden md:hidden lg:block">
             Trending
           </h5>
         </div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
       <PopularMovies />
     </>
